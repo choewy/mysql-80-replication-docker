3cc480819365b1aa710ec4e0c82e4a5411ad5348
choewy <choewy32@gmail.com>
2023-08-14 00:30:11 +0900

    refactor/api module ref

--- src/apis/apis.refs.ts
+++ src/apis/apis.refs.ts
@@ -1,9 +1,7 @@
 import { forwardRef } from '@nestjs/common';
 
 import { ApiKeyModule } from './api-key';
-import { NaverApiModule } from './naver-api';
 
 export class ApisModuleRef {
   public static readonly ApiKeyModule = forwardRef(() => ApiKeyModule);
-  public static readonly NaverApiModule = forwardRef(() => NaverApiModule);
 }
